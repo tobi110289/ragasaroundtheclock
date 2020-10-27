5f776e93c16b45f23c6625f59e0f6767e01a7594
tobi110289 <tobias.feltes@web.de>
2020-10-27 08:24:07 +0100

    first testbuild

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import { Component } from 'react';
+import bgImage from './assets/bg.png';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { time: new Date(Date.now) };
+  }
+
+  timeCheck (hour) {
+    let selector = 'earlymorning';
+    switch (hour) {
+      case (hour < 3): selector = 'latenight';
+      case (hour >= 3 && hour < 6): selector = 'earlymorning';
+      case (hour >= 6 && hour < 9): selector = 'morning';
+      case (hour >= 9 && hour < 12): selector = 'forenoon';
+      case (hour >= 12 && hour < 15): selector = 'afternoon';
+      case (hour >= 15 && hour < 18): selector = 'evening';
+      case (hour >= 18 && hour < 21): selector = 'lateevening';
+      case (hour >= 21 && hour < 24): selector = 'night';
+        break;
+    }
+    return selector;
+  }
+
+  render () {
+    return (<div style={{
+      backgroundImage: `url(${bgImage})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    }} className="container">
+      <h1>Ragas Around The Clock</h1>
+      <span id="earlymorning">Early Morning: 3am to 6am</span>
+      <iframe src="https://open.spotify.com/embed/playlist/0qOibXTlBLpx3jA8S5isVy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="morning">Morning: 6am to 9am</span>
+      <iframe src="https://open.spotify.com/embed/playlist/5H9eS9VuerMbl66utlc4ok" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="forenoon">Forenoon: 9am to 12pm</span>
+      <iframe src="https://open.spotify.com/embed/playlist/0vAO3i2PVvRG9S31gcenxC" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="afternoon">Afternoon: 12pm to 3pm</span>
+      <iframe src="https://open.spotify.com/embed/playlist/56yTG7BTH4JjHASV4JGmnl" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="evening">Evening: 3pm to 6pm</span>
+      <iframe src="https://open.spotify.com/embed/playlist/0vfjqbbpV97lvPxCkEK3u7" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="lateevening">Late Evening: 6pm to 9pm</span>
+      <iframe src="https://open.spotify.com/embed/playlist/2J42xrLTNM2pmCGKmaigd3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="night">Night: 9pm to 12am</span>
+      <iframe src="https://open.spotify.com/embed/playlist/5RZ1rK5oZ6LtKwfvH8bvhR" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <span id="latenight">Late Night: 12am to 3am</span>
+      <iframe src="https://open.spotify.com/embed/playlist/3a8Cfn9sAaVKuA0v3YZ4Mi" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </div>
+    );
+  }
+}
+
+export default App;
